8126210709d6cf3b3472f1363e9f134b1e40000d
Jonas Gilg <jonas.gilg@dlr.de>
2019-08-19 15:10:50 +0200

    :hammer: UI now works with Bootstrap instead of Materialize.

--- gui/js/fly-to-locations-tab.js
+++ gui/js/fly-to-locations-tab.js
@@ -1,59 +1,64 @@
 function add_location(group, text) {
-    const tab_name = 'list-location-' + group;
-    const list = $('#' + tab_name);
-    const area = $('#location-tabs-area');
-
-    if (area.children().length === 0) {
-        area.append('<div class=\'col s12\'> \
-                        <ul class=\'tabs\' id=\'location-tabs\'> \
-                        </ul> \
-                    </div>');
+    let first = false;
+    const tabArea = $("#location-tabs-area");
+    if (tabArea.children().length === 0) {
+        first = true;
+        tabArea.append(`
+            <nav>
+                <div class="row nav nav-tabs" id="location-tabs" role="tablist"></div>
+            </nav>
+            <div class="tab-content" id="nav-tabContents"></div>
+        `)
     }
 
-    if (list.length === 0) {
-        const tabs = $('#location-tabs');
-        tabs.children().removeAttr('style');
-        tabs.append('<li class=\'tab col s3\'><a href=\'#' + tab_name + '\'>' + group + '</a></li>');
-        area.append('\
-            <div class="col s12 scroll-box" style="max-height:250px">\
-                <div id="' +
-            tab_name + '" class="item-list scroll-box-content">\
-                </div>\
-            </div>\
-        ');
+    const locationsTab = $("#location-tabs");
+    const tabContents = $("#nav-tabContents");
 
-        // initalize tabs again, then select new tab
-        tabs.tabs();
+    let groupTab = $(`#nav-${group}`);
+    if (groupTab.length === 0) {
+        const active = first ? "active" : "";
+        locationsTab.append(`
+            <a class="ripple nav-item nav-link ${active} col-3" id="nav-${group}-tab" data-toggle="tab" href="#nav-${group}" 
+                role="tab" aria-controls="nav-${group}" aria-selected="${first}">${group}</a>
+        `);
+
+        const show = first ? "show" : "";
+
+        tabContents.append(`
+            <div class="tab-pane fade ${show} ${active}" id="nav-${group}" role="tabpanel" aria-labelledby="nav-${group}-tab"></div>
+        `);
+
+        groupTab = $(`#nav-${group}`);
     }
 
-    const html = '\
-        <div class=\'row\'>\
-            <div class=\'col s8\'>\
-                ' +
-        text + '\
-            </div>\
-            <div class=\'col s4\'><a class=\'waves-effect waves-light btn glass tooltipped block\' data-position=\'top\' data-delay=\'50\' data-tooltip=\'Fly to ' +
-        text + '\' onclick=\'window.call_native("fly_to", "' + text +
-        '")\' ><i class=\'material-icons\'>send</i></a></div>\
-   </div>';
+    groupTab.append(`
+        <div class='row'>
+            <div class='col-8'>
+                ${text}
+            </div>
+            <div class='col-4'>
+                <a class='ripple btn glass block' data-toggle="tooltip" title='Fly to ${text}' onclick='window.call_native("fly_to", "${text}")'>
+                    <i class='material-icons'>send</i>
+                </a>
+            </div>
+        </div>
+    `);
 
-    $('#' + tab_name).append(html);
+    $('[data-toggle="tooltip"]').tooltip({delay: 500, placement: "auto", html: false});
 }
 
 
 function add_celestial_body(name, icon) {
     const area = $('#celestial-bodies');
-    area.append('<div class=\'col s3 center\'>\
-                    <label>\
-                        <input id=\'set_body_' +
-        name + '\' type=\'radio\' name=\'celestial-body\' />\
-                        <div class=\'waves-effect waves-light block btn btn-large glass\'>\
-                            <img style=\'pointer-events: none\' src=\'../icons/' +
-        icon + '\' height=\'80\' width=\'80\'>' + name + '</div>\
-                    </label>\
-                </div>');
+    area.append(`<div class='col-3 center'>
+                    <label>
+                        <input id='set_body_${name}' type='radio' name='celestial-body' />
+                        <div class='ripple block btn glass'>
+                            <img style='pointer-events: none' src='../icons/${icon}' height='80' width='80'>${name}</div>
+                    </label>
+                </div>`);
 
-    $('#set_body_' + name).on('click', function() {
+    $('#set_body_' + name).on('click', function () {
         window.call_native('set_celestial_body', name);
     });
 }
