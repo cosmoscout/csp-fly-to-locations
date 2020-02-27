9c7783b2b8b7ea98cce062d267322267b8dd9906
tomviehweger <61310764+tomviehweger@users.noreply.github.com>
2020-02-27 15:16:44 +0100

    Update gui/js/csp-fly-to-locations.js
    Co-Authored-By: Jonas Gilg <jonas@gilg-hannover.de>

--- gui/js/csp-fly-to-locations.js
+++ gui/js/csp-fly-to-locations.js
@@ -25,7 +25,7 @@ class FlyToApi extends IApi {
     iconSize:     [10, 10], // size of the icon
     iconAnchor:   [5, 5], // point of the icon which will correspond to marker's location
     popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
-  })
+  });
   
 // The active planet.
   activePlanet = null;
